86de217ff4a985cbd74772e3f2bf5bb2d6649872
Shivansh Sharma <shivansh.sharma706@gmail.com>
2022-07-28 10:00:18 +0530

    all resolved

--- Script.js
+++ Script.js
@@ -1,3 +1,56 @@
+var myAudio = new Audio('b.mp3')
+
+function load(){
+  myAudio.volume = 0.05
+  myAudio.play();
+  if (typeof myAudio.loop == 'boolean')
+  {
+      myAudio.loop = true;
+  }
+  else
+  {
+      myAudio.addEventListener('ended', function() {
+          this.currentTime = 0;
+          this.play();
+      }, false);
+  }
+}
+
+document.querySelector('.audioWaves').addEventListener('click', (e)=>{
+  if(!myAudio.paused){
+      document.querySelectorAll('.loading .x').forEach(element => {
+              element.style.animationPlayState = 'paused'
+      });
+      var vol = 0.05
+      var setup = setInterval(() => {
+          if (vol >= 0) {
+              myAudio.volume = vol
+              vol -= 0.01
+          }
+          else{
+              myAudio.pause()
+              clearInterval(setup)
+          }
+      }, 100);
+  }
+  else{
+      document.querySelectorAll('.loading .x').forEach(element => {
+              element.style.animationPlayState = 'running'
+      });
+      var vol = 0.0
+      myAudio.play()
+      var setup = setInterval(() => {
+          if (vol <= 0.08) {
+              myAudio.volume = vol
+              vol += 0.01
+          }
+          else{
+              clearInterval(setup)
+          }
+      }, 100);
+  }
+
+})
 
 snowFall.snow(document.getElementsByClassName("snow"),{
     round:true,
@@ -24,9 +77,13 @@ function carousel() {
   setTimeout(carousel, 300000);
 }
 
-window.onload = function() {
-    document.getElementById("audioplayer").play();
-}
+// window.onload = function() {
+//     document.getElementById("audioplayer").play();
+// }
+
+
+
+
 
 // var source = "b.mp3"
 // var audio = document.createElement("audio");
